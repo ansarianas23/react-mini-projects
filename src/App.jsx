a7e2ce5cc4d399e0ed96683689bcd76751690531
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-01-30 21:13:03 +0530

    "added Project List"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react"
+import Header from "./components/Header"
 import ImageSlider from "./components/ImageSlider"
+import OTPInput from "./components/OTPInput";
+import ActiveTab from "./components/ActiveTab";
+import PillSelectorInput from "./components/PillSelectorInput";
 
 function App() {
 
+  const [currentProject, setCurrentProject] = useState("Image Slider");
+  const projectList = [
+    "Image Slider",
+    "OTP Input",
+    "Active Tab",
+    "Pill Selector Input",
+  ];
+
+
   return (
     <>
-    <ImageSlider/>
+    <div>
+      <Header currentProject={currentProject} setCurrentProject={setCurrentProject} projectList={projectList}/>
+    </div>
+
+    <div className="w-full">
+      {currentProject === "Image Slider" && <ImageSlider/>}
+      {currentProject === "OTP Input" && <OTPInput/>}
+      {currentProject === "Active Tab" && <ActiveTab/>}
+      {currentProject === "Pill Selector Input" && <PillSelectorInput/>}
+    </div>
     </>
   )
 }
