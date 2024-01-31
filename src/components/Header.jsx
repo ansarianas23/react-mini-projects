92eaa8cacf3dfd2f910b00cc28f41a104573d087
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-01-31 15:29:58 +0530

    "Hamburger Menu is Created"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 
 const Header = ({currentProject, setCurrentProject, projectList}) => {
-
+  const [toggleMenu, setToggleMenu] = useState(false);
 
 
   return (
-    <div className='sticky top-0 w-full h-12 flex items-center px-4 shadow-md'>
-      <ul className='flex space-x-4'>
+    <div className='sticky top-0 w-full flex items-center h-12 px-4 shadow-md z-20'>
+      <span onClick={()=>{setToggleMenu(true)}} className='md:hidden text-xl'><FaBars /></span>
+      <ul className={`w-[50%] md:w-fit md:h-full h-screen absolute top-0 left-0 p-5 md:p-0 space-y-4 md:space-y-0 bg-white text-black md:flex md:flex-row md:items-center flex-col md:space-x-4 shadow-lg md:shadow-none ${toggleMenu? "":"-translate-x-full md:translate-x-0"}  transition-transform`}>
+
+        <span onClick={()=>setToggleMenu(false)} className='absolute text-3xl right-3 md:hidden'><IoMdClose /></span>
         {
           projectList.map((listItem)=>{
             return  <li 
